efa6ecfc68e81c830d28a7841f009f2a2e0ebe87
Your Name <your-email@example.com>
2025-03-10 15:06:11 +0600

    Initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+TOKEN='7913499758:AAFYMijL01K9m4COGFAXMj-cSX429YAXrlc'
+DB_URL='sqlite+aiosqlite:///db.sqlite3'
+#DB_URL='postgresql+asyncpg://user:password@host:port/dbname[?key=value&key=value...]'
+AI_TOKEN= 'A7sAvkwiSntQi0RWBaZ41rgZfNN18xtX'
+# Внимание! Добавьте config.py в файл .gitignore!
